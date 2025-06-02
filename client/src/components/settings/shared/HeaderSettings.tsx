96ef1dda10bde72c170f532e0a903885009cf125
diego <diego.roque0309@gmail.com>
2025-06-02 14:59:25 -0600

    HeaderSettings.tsx

--- client/src/components/settings/shared/HeaderSettings.tsx
+++ client/src/components/settings/shared/HeaderSettings.tsx
@@ -4,7 +4,6 @@ function Header_settings()  {
     return (
         <div className="flex justify-between items-center mb-6">
             <h1 className="text-3xl font-bold pl-8">Configuración de la cuenta</h1>
-
         </div>
     );
 }
