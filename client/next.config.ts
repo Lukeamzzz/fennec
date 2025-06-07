1a4cde76b492d8ec1ad08a72ac66d15940703874
diego <diego.roque0309@gmail.com>
2025-06-07 01:20:50 -0600

    next config

--- client/next.config.ts
+++ client/next.config.ts
@@ -3,9 +3,19 @@ import type { NextConfig } from "next";
 const securityHeaders = [
     {
         key: "Content-Security-Policy",
-        value:
-            "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self'; object-src 'none'; base-uri 'self'; frame-ancestors 'none'; form-action 'self'; img-src 'self'; font-src 'self';",
-
+        value: `
+      default-src 'self';
+      script-src 'self' 'unsafe-inline' blob: https://api.mapbox.com https://events.mapbox.com;
+      style-src 'self' 'unsafe-inline' https://api.mapbox.com https://fonts.googleapis.com;
+      object-src 'none';
+      base-uri 'self';
+      frame-ancestors 'none';
+      form-action 'self';
+      img-src 'self' data: blob: https://api.mapbox.com;
+      font-src 'self' https://fonts.gstatic.com;
+      connect-src 'self' http://localhost:8080 https://identitytoolkit.googleapis.com https://api.mapbox.com https://events.mapbox.com;
+      worker-src 'self' blob:;
+    `.replace(/\s{2,}/g, ' ').trim(),
     },
     {
         key: "X-Frame-Options",
