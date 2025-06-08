f27f07361a47ecfce75406cd329845fad9f07c36
diego <diego.roque0309@gmail.com>
2025-06-08 16:27:53 -0600

    prueba next config anti mapa bruno

--- client/next.config.ts
+++ client/next.config.ts
@@ -4,18 +4,19 @@ const securityHeaders = [
     {
         key: "Content-Security-Policy",
         value: `
-        default-src 'self';
-        script-src 'self';
-        style-src 'self';
-      object-src 'none';
-      base-uri 'self';
-      frame-ancestors 'none';
-      form-action 'self';
-      img-src 'self' data: blob: https://api.mapbox.com;
-      font-src 'self' https://fonts.gstatic.com;
-      connect-src 'self' https://fennec-back-447938427814.northamerica-south1.run.app:8080 https://identitytoolkit.googleapis.com https://api.mapbox.com https://events.mapbox.com https://fennec-prediccion.onrender.com https://securetoken.googleapis.com;
-      worker-src 'self' blob:;
-    `.replace(/\s{2,}/g, ' ').trim(),
+  default-src 'self';
+  script-src 'self';
+  style-src 'self';
+  object-src 'none';
+  base-uri 'self';
+  frame-ancestors 'none';
+  form-action 'self';
+  img-src 'self';
+  font-src 'self';
+  connect-src 'self' https://fennec-back-447938427814.northamerica-south1.run.app:8080;
+  worker-src 'self' blob:;
+`.replace(/\s{2,}/g, ' ').trim(),
+
     },
     {
         key: "X-Frame-Options",
