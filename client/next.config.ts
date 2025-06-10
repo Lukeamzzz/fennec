d7cdc97536cbdec700c8e6349c97e8d2dc0c99de
Gerardo AM <128639709+Lukeamzzz@users.noreply.github.com>
2025-06-09 20:13:34 -0600

    Update next.config.ts

--- client/next.config.ts
+++ client/next.config.ts
@@ -10,7 +10,7 @@ const ContentSecurityPolicy = `
   form-action 'self';
   img-src 'self';
   font-src 'self';
-  connect-src 'self' https://fennec-back-deploy-447938427814.northamerica-south1.run.app https://identitytoolkit.googleapis.com; https://fennec-prediccion.onrender.com
+  connect-src 'self' https://fennec-back-deploy-447938427814.northamerica-south1.run.app https://identitytoolkit.googleapis.com; https://fennec-prediccion.onrender.com;
   worker-src 'none';
 `.replace(/\s{2,}/g, " ").trim();
 
